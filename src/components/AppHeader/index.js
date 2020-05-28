4633cf8c49f9a52c2806658621b2ef4b2b66d5e2
Berkay <berkaydemirbas@yahoo.com>
2020-05-28 16:40:11 +0300

    Header fixed

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -14,7 +14,7 @@ const AppHeader = (props) => {
             <div className='app-title-text'>
               <h1 className='app-title'>Netlify + Fauna DB</h1>
               <p className='app-intro'>
-                Using FaunaDB & Netlify functions
+               JAMstack Istanbul Netlif + Fauna DB
               </p>
             </div>
           </div>
@@ -23,18 +23,9 @@ const AppHeader = (props) => {
           <a
             target='_blank'
             rel='noopener noreferrer'
-            href='https://app.netlify.com/start/deploy?repository=https://github.com/netlify/netlify-faunadb-example&stack=fauna'>
+            href='https://app.netlify.com/start/deploy?repository=https://github.com/raufsamestone/netlify-faunadb-example&stack=fauna'>
             <img src={deployButton} className='deploy-button' alt='deploy to netlify' />
           </a>
-          <div className='view-src'>
-            <a
-              target='_blank'
-              rel='noopener noreferrer'
-              href='https://github.com/netlify/netlify-faunadb-example'>
-              <img className='github-icon' src={github} alt='view repo on github' />
-              View the source Luke
-            </a>
-          </div>
         </div>
       </div>
     </header>
